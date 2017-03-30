a944135e1ee37376243881ef10855043c9da4698
kanato1107 <dedicatus1107@gmail.com>
2017-03-31 05:59:06 +0900

    完成版

---
+++ Assets/Scripts/Debug.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug : MonoBehaviour {
+
+    // ボール
+    [SerializeField]
+    GameObject ball;
+
+    // ボールの座標
+    private Vector3 m_ballPos;
+
+	// Use this for initialization
+	void Start () {
+
+        // ボールの座標を取得
+        m_ballPos = ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        // ボールの座標を取得
+        m_ballPos = ball.transform.position;
+
+        // ボールに100％Y軸を追従させる【デバッグ】
+        transform.position = new Vector3(transform.position.x, m_ballPos.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/Title/Title.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // SPACEボタンでゲームシーンへ移動
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Game");
+        }
+	}
+}
